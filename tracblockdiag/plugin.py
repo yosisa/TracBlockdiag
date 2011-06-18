a4d30adaa241c1e5dfae691641e7772093df8242
Yoshihisa Tanaka <yt.hisa@gmail.com>
2011-06-18 18:54:16 +0900

    add configurations by trac.ini

--- tracblockdiag/plugin.py
+++ tracblockdiag/plugin.py
@@ -9,20 +9,24 @@ from trac.web import IRequestHandler
 from trac.wiki import IWikiMacroProvider
 from trac.wiki.formatter import system_message
 
-from .diag import detectfont, get_diag
+from .diag import detectfont, get_diag, memoize
 
 macro_defs = {'blockdiag': 'description'}
 content_types = {'png': 'image/png',
                  'svg': 'image/svg+xml'}
+_conf_section = 'tracblockdiag'
 
 
 class BlockdiagRenderer(Component):
     implements(IWikiMacroProvider, IRequestHandler)
 
     def __init__(self):
-        self.font = detectfont()
+        preferfont = self.config.getlist(_conf_section, 'font')
+        cachetime = self.config.getint(_conf_section, 'cachetime', 300)
+        self.font = detectfont(preferfont)
         self.url = re.compile(r'/blockdiag/([a-z]+)/(png|svg)/(.+)')
         self.src = 'blockdiag/%(type)s/%(fmt)s/%(data)s'
+        self.get_diag = memoize(cachetime)(get_diag)
 
     def get_macros(self):
         return macro_defs.keys()
@@ -42,5 +46,5 @@ class BlockdiagRenderer(Component):
 
     def process_request(self, req):
         type_, fmt, data = self.url.match(req.path_info).groups()
-        diag = get_diag(b64decode(data), fmt, self.font)
+        diag = self.get_diag(b64decode(data), fmt, self.font)
         req.send(diag, content_types.get(fmt.lower(), ''), status=200)
