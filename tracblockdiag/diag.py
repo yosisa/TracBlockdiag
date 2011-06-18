abda353ad3755ce0319120f6bf741f39539334d6
Yoshihisa Tanaka <yt.hisa@gmail.com>
2011-06-18 17:43:20 +0900

    add cache for rendered diagrams

--- tracblockdiag/diag.py
+++ tracblockdiag/diag.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import os
+import time
+import hashlib
+import pickle
 
 from blockdiag.diagparser import parse, tokenize
 from blockdiag.builder import ScreenNodeBuilder
@@ -10,6 +13,31 @@ try:
     from cStringIO import StringIO
 except ImportError:
     from StringIO import StringIO
+
+_cache = {}
+
+
+def is_obsolete(entry, duration):
+    return time.time() - entry['time'] > duration
+
+
+def compute_key(function, args, kwargs):
+    key = pickle.dumps((function.func_name, args, kwargs))
+    return hashlib.sha1(key).hexdigest()
+
+
+def memoize(duration=10):
+    def _memoize(function):
+        def __memoize(*args, **kwargs):
+            key = compute_key(function, args, kwargs)
+            if key in _cache and not is_obsolete(_cache[key], duration):
+                _cache[key]['time'] = time.time()
+                return _cache[key]['value']
+            result = function(*args, **kwargs)
+            _cache[key] = {'value': result, 'time': time.time()}
+            return result
+        return __memoize
+    return _memoize
 
 
 def detectfont(prefer=None):
@@ -25,6 +53,7 @@ def detectfont(prefer=None):
             return font
 
 
+@memoize(300)
 def get_diag(text, fmt, font=None, antialias=True, nodoctype=False):
     if not isinstance(fmt, basestring):
         return None
