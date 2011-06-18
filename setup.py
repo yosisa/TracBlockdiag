5e73b9e5e48e15d7e51991e6f5a2175da14d36a3
Yoshihisa Tanaka <yt.hisa@gmail.com>
2011-06-18 20:46:05 +0900

    add extras_require to install all *diag

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(name='TracBlockdiagPlugin',
       version='0.1.0',
       packages=find_packages(exclude=['*.tests*']),
       install_requires=['blockdiag>=0.8.1'],
+      extras_require={
+          'full': ['seqdiag>=0.3.5', 'actdiag>=0.1.5', 'nwdiag>=0.2.4']
+          },
       entry_points = """
       [trac.plugins]
       tracblockdiag = tracblockdiag.plugin
