af20f5f7d3de48ef44f0e899d3fb5481a7ec4511
Yoshihisa Tanaka <yt.hisa@gmail.com>
2011-06-19 11:26:48 +0900

    rename package name

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-setup(name='TracBlockdiagPlugin',
+setup(name='TracBlockdiag',
       version='0.1.0',
       packages=find_packages(exclude=['*.tests*']),
       install_requires=['blockdiag>=0.8.1'],
