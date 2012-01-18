eaf7d4ca36fdeea01202fcf47169379808f63be9
Yoshihisa Tanaka <yt.hisa@gmail.com>
2012-01-18 17:30:46 +0900

    fix tests

--- tests/test_diag.py
+++ tests/test_diag.py
@@ -15,7 +15,7 @@ def skipUnlessImport(module):
         return unittest.skip("module not found: {0}".format(module))
 
 
-class BlockdiagBuildMixin(object):
+class TestBlockdiagBuilder(unittest.TestCase):
     png_header = (0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A)
     blockdiag_text = """
     {
@@ -29,6 +29,7 @@ class BlockdiagBuildMixin(object):
             'antialias': False,
             'nodoctype': False
         }
+        self.builder = diag.BlockdiagBuilder
 
     def test_attr(self):
         self.assertTrue(hasattr(self.builder, 'parse_string'))
@@ -36,38 +37,26 @@ class BlockdiagBuildMixin(object):
         self.assertTrue(hasattr(self.builder, 'DiagramDraw'))
 
     def test_build_png(self):
-        png = self.builder.build(self.blockdiag_text, 'PNG',
-                                 self.default_options)
+        png = self.builder().build(self.blockdiag_text, 'PNG',
+                                   self.default_options)
         self.assertEqual(png[:8], ''.join(chr(x) for x in self.png_header))
 
     def test_build_svg(self):
-        svg = self.builder.build(self.blockdiag_text, 'SVG',
-                                 self.default_options)
+        svg = self.builder().build(self.blockdiag_text, 'SVG',
+                                   self.default_options)
         dom = minidom.parseString(svg)
         self.assertEqual(dom.doctype.name, 'svg')
 
 
-@skipUnlessImport('blockdiag.parser')
-class TestBlockdiagBuilder(BlockdiagBuildMixin, unittest.TestCase):
-    def setUp(self):
-        super(TestBlockdiagBuilder, self).setUp()
-        self.builder = diag.BlockdiagBuilder('blockdiag')
+class TestFunctions(unittest.TestCase):
+    def test_builder_generation(self):
+        for name in diag.available_builders:
+            self.assertTrue(hasattr(diag, name.title() + 'Builder'))
 
-
-@skipUnlessImport('blockdiag.diagparser')
-class TestLegacyBlockdiagBuilder(BlockdiagBuildMixin, unittest.TestCase):
-    def setUp(self):
-        super(TestBlockdiagBuilder, self).setUp()
-        self.builder = diag.LegacyBlockdiagBuilder('blockdiag')
-
-
-class TestBlockdiagLoader(unittest.TestCase):
-    def setUp(self):
-        diag.BlockdiagLoader.search_builders = ['blockdiag']
-        self.loader = diag.BlockdiagLoader()
-
-    def test_available_builders(self):
-        self.assertEqual(self.loader.available_builders(), ['blockdiag'])
+    def test_get_builder(self):
+        self.assertEqual(diag.get_builder('block'), diag.BlockdiagBuilder)
+        with self.assertRaises(AttributeError):
+            diag.get_builder('notexists')
 
 
 if __name__ == '__main__':
